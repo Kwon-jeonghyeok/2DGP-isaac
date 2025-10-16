df1b4ddb98d02dcc8c3d85ed0277253f7f142074
jeonghyeokkwon <jhkwon036@gmail.com>
2025-10-16 21:07:46 +0900

    IDLE (정지상태, 애니메이션 없음) 그리기 추가

--- isaac.py
+++ isaac.py
@@ -5,23 +5,26 @@ from state_machine import StateMachine
 class Idle:
     def __init__(self, isaac):
         self.isaac = isaac
+        self.frame_counter = 0
     def enter(self,e):
         self.isaac.dir = 0
 
     def exit(self,e):
         pass
     def do(self):
-        self.isaac.frame = (self.isaac.frame + 1) % 2
+        self.frame_counter += 1
+        if self.frame_counter % 10 == 0:
+            self.isaac.frame = (self.isaac.frame + 1) % 2
         pass
     def draw(self):
+        # 몸통
+        self.isaac.image.clip_draw(0, 850, 40, 30, self.isaac.x, self.isaac.y - 35, 90, 80)
         #머리
-        self.isaac.image.clip_draw(self.isaac.frame * 35, 900, 35, 35, self.isaac.x, self.isaac.y,100,100)
-        #몸통
-        self.isaac.image.clip_draw(35, 870, 35, 35, self.isaac.x, self.isaac.y-35, 100, 100)
+        self.isaac.image.clip_draw(0, 900, 40, 35, self.isaac.x, self.isaac.y,90,75)
 
 class Isaac:
     def __init__(self):
-        self.x, self.y = 400,300
+        self.x, self.y = 500,300
         self.frame = 0
         self.dir = 0
         self.face_dir = 1
