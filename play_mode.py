4f67f5d056c3b30223cad0bf0f98b54bd4ab31ae
jeonghyeokkwon <jhkwon036@gmail.com>
2025-11-06 18:49:30 +0900

    play_mode 추가

---
+++ play_mode.py
@@ -0,0 +1,44 @@
+from pico2d import *
+import game_framework
+import game_world
+
+from isaac import Isaac
+from stage_1 import Stage_1
+
+isaac =None
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            isaac.handle_event(event)
+
+def init():
+
+    stage = Stage_1()
+    game_world.add_object(stage,0)
+
+    global isaac
+    isaac = Isaac()
+    game_world.add_object(isaac,1)
+
+
+def update():
+    game_world.update()
+    pass
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def finish():
+    game_world.clear()
+    pass
+
+def pause(): pass
+def resume(): pass
