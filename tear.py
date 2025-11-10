ea95ccaa3a705236670374e83a3ebaa59879d220
jeonghyeokkwon <jhkwon036@gmail.com>
2025-11-10 14:48:25 +0900

    눈물 애니메이션 초기 설정

--- tear.py
+++ tear.py
@@ -3,16 +3,18 @@ import game_world
 import game_framework
 
 PIXEL_PER_METER = (1.0 / 0.03)
-
+TIME_PER_ACTION = 1.0
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 10
 class Tear:
     image = None
-    def __init__(self, x=400, y=300, velocity=1):
+    def __init__(self, x=400, y=300, velocity=0.5):
         if Tear.image == None:
             Tear.image = load_image('resourse/objects/tears.png')
         self.x, self.y, self.velocity = x, y, velocity
 
     def draw(self):
-        self.image.clip_draw(0, 300, 40, 35, self.x, self.y, 90, 75)
+        self.image.clip_draw(252,215, 45, 40, self.x, self.y, 60, 60)
 
     def update(self):
         self.x += self.velocity
