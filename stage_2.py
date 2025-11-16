22edd9692dbd86557eef7b4465dcdeb2f94ca8c3
Kwon jeonghyeok <jhkwon036@gmail.com>
2025-11-17 01:39:09 +0900

    스테이지2에 1로 가는 문 그리기 추가

--- stage_2.py
+++ stage_2.py
@@ -4,6 +4,7 @@ from pico2d import load_image
 class Stage_2:
     def __init__(self):
         self.image = load_image('resource/rooms/Rooms_Basement-1.png')
+        self.image2 = load_image('resource/objects/Door_1.png')
 
     def get_map_bounds(self):
         return {
@@ -27,7 +28,8 @@ class Stage_2:
         self.image.composite_draw(0,'h',750,600,500,400)
         self.image.composite_draw(0,'v',250,200,500,400)
         self.image.composite_draw(0,'hv',750,200,500,400)
-
+        self.image2.clip_composite_draw(0, 40, 50, 52,0,'v' ,500, 120, 120, 120)
+        self.image2.clip_composite_draw(50, 40, 50, 52,0,'v', 465, 120, 130, 120)
 
 
         pass
