66c56bd141a9f0963fdb816db804023f44ac3653
Kwon jeonghyeok <jhkwon036@gmail.com>
2025-11-16 22:45:42 +0900

    충돌처리 로직 추가

--- game_world.py
+++ game_world.py
@@ -15,6 +15,9 @@ def remove_object(o):
     raise Exception("World 에 존재하지 않는 오브젝트를 지우려고 시도함")
 
 def clear():
+
+    global world
+
     for layer in world:
         layer.clear()
 def update():
@@ -27,4 +30,52 @@ def render():
         for o in layer:
             o.draw()
 
+
+
+collision_pairs = {}
+def add_collision_pair(group, a, b):
+    if group not in collision_pairs:
+        print(f'Add new pair: {group}')
+        collision_pairs[group] = [[], []]
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
+
+def handle_collision():
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide(a, b):
+                    a.handle_collision(group, b)  # group :충돌정보, b : 대상
+                    b.handle_collision(group, a)
+
+# 충돌검사
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
+# 객체의 충돌정보 삭제
+def remove_collision_object(o):
+    for pairs in collision_pairs.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            remove_collision_object(o)
+            return
+
+    raise ValueError('Cannot delete non existing object')
 
