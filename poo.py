152de4eb6502c64a6b0eb73b01c60695ac57181c
jeonghyeokkwon <jhkwon036@gmail.com>
2025-12-04 12:29:30 +0900

    똥이 눈물을 4대 맞으면 충돌이 사라지게 구현, 이미지 구현

--- poo.py
+++ poo.py
@@ -4,18 +4,23 @@ import game_world
 
 class Poo:
     image = None
+    FRAMES = 5
 
     def __init__(self, x, y, w=40, h=40):
         if Poo.image is None:
             try:
-                Poo.image = load_image('resource/objects/poo.png')
+                Poo.image = load_image('resource/objects/Poop.png')
             except Exception:
                 Poo.image = None
         self.x = float(x)
         self.y = float(y)
         self.w = float(w)
         self.h = float(h)
-        self.destroyed = False
+
+        self.frame = 0           # 현재 표시할 프레임 (0..4)
+        self.hit_count = 0       # 맞은 횟수
+        self.collidable = True   # 충돌 판정 활성화 여부
+        self.destroyed = False   # 완전 제거 플래그 (필요시 사용)
 
     def update(self):
         # 정적 오브젝트
@@ -26,40 +31,40 @@ class Poo:
             return
         sx, sy = game_world.world_to_screen(self.x, self.y)
         if Poo.image:
-            Poo.image.draw(sx, sy, self.w, self.h)
+            fw = int(Poo.image.w // Poo.FRAMES)
+            fh = int(Poo.image.h)
+            Poo.image.clip_draw(self.frame * fw, 0, fw, fh, sx, sy, self.w, self.h)
         else:
             draw_rectangle(sx - self.w / 2, sy - self.h / 2,
                            sx + self.w / 2, sy + self.h / 2)
 
     def get_bb(self):
-        if self.destroyed:
-            # 이미 파괴된 경우, 충돌 안 나도록 0 영역 반환
+        # 충돌 판정이 비활성화된 경우 0 영역 반환
+        if not self.collidable or self.destroyed:
             return 0, 0, 0, 0
         return (self.x - self.w / 2, self.y - self.h / 2,
                 self.x + self.w / 2, self.y + self.h / 2)
+
     def destroy(self):
-        # 월드와 충돌쌍에서 제거
         try:
             game_world.remove_object(self)
         except Exception:
             pass
         self.destroyed = True
 
-
     def handle_collision(self, group, other):
-        # 오직 눈물 그룹과 충돌할 때만 제거
         if ':tear' in group:
-            if self.destroyed:
+            if not self.collidable or self.destroyed:
                 return
-            self.destroy()
-            try:
-                game_world.remove_collision_object(self)
-            except Exception:
-                pass
-            try:
-                game_world.remove_object(self)
-            except Exception:
-                pass
+            # 히트 처리: 프레임 증가
+            self.hit_count += 1
+            # 프레임은 0..FRAMES-1
+            self.frame = min(self.hit_count, Poo.FRAMES - 1)
+            # 마지막 프레임(인덱스 FRAMES-1) 이후에는 충돌 판정만 제거
+            if self.hit_count >= Poo.FRAMES - 1:
+                self.collidable = False
+                try:
+                    game_world.remove_collision_object(self)
+                except Exception:
+                    pass
         return
-
-
